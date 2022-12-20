e1073323f500ea6957826b2a467aa768aa2a85bf
Astr0-G <wangge326@gmail.com>
2022-12-19 20:13:28 -0800

    updated

--- constants/erc1155mumbai.js
+++ constants/erc1155mumbai.js
@@ -4,7 +4,7 @@ export const erc1155mumbai = [
         chain: 80001,
         symbol: "matic",
         name: "Nyan cat",
-        address: "0xec1254f8ecf5137f04b7079464A656b82f163Fc0",
+        address: "0x05fdBac96C17026c71681150aa44Cbd0DDDd3374",
         pic: "https://i.seadn.io/gcs/files/6564981c9bed29b375a40e51ef6f813c.gif?auto=format&w=1920",
         scan: "https://goerli.etherscan.io/address/",
     },
