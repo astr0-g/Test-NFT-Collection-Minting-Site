82acf3cb14f0a86bb38651bcb9a111231bef19a9
wg326 <57165451+wg326@users.noreply.github.com>
2023-09-06 13:27:53 -0700

    re org jsonRpcProvider

--- pages/_app.js
+++ pages/_app.js
@@ -21,32 +21,67 @@ import {
     walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets"
 
+const INFURAKEYLIST = [
+    process.env.NEXT_PUBLIC_INFURAAPIKEY1,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY2,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY3,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY4,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY5,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY6,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY7,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY8,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY9,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY10,
+    process.env.NEXT_PUBLIC_INFURAAPIKEY11,
+]
+
+const getRandomKey = () => {
+    const randomIndex = Math.floor(Math.random() * INFURAKEYLIST.length)
+    return INFURAKEYLIST[randomIndex]
+}
+
 const { chains, provider } = configureChains(
     [chain.sepolia, chain.goerli, chain.polygonMumbai],
     [
         // publicProvider(),
         jsonRpcProvider({
-            rpc: (chain) => ({
-                http: process.env.NEXT_PUBLIC_SOPOLIAURL,
-            }),
+            rpc: (chain) => {
+                switch (chain.id) {
+                    case 5:
+                        return {
+                            http: `https://goerli.infura.io/v3/${getRandomKey()}`,
+                        }
+                    case 11155111:
+                        return {
+                            http: `https://sepolia.infura.io/v3/${getRandomKey()}`,
+                        }
+                    case 80001:
+                        return {
+                            http: `https://polygon-mumbai.infura.io/v3/${getRandomKey()}`,
+                        }
+                    default:
+                        throw new Error(`Unsupported chain: ${chain.id}`)
+                }
+            },
         }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY1, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY2, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY3, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY4, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY5, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY6, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY7, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY8, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY9, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY10, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY11, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY12, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY13, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY14, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY15, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY16, priority: 0 }),
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY17, priority: 0 }),
+
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY1, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY2, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY3, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY4, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY5, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY6, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY7, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY8, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY9, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY10, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY11, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY12, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY13, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY14, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY15, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY16, priority: 0 }),
+        // alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMYAPIKEY17, priority: 0 }),
         // infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURAAPIKEY1, priority: 0 }),
         // infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURAAPIKEY2, priority: 0 }),
         // infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURAAPIKEY3, priority: 0 }),
