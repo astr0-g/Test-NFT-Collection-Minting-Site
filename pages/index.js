6acfeee2fa5a0aa112595aa5d2529515ebefe93b
wg326 <57165451+wg326@users.noreply.github.com>
2023-09-06 11:44:42 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,11 @@ export default function Home() {
                 <title>Testnet Minting Site</title>
                 <meta name="description" content="Ethereum testnet minting site" />
                 <link rel="icon" href="/Cjdowner-Cryptocurrency-Flat-Ethereum-ETH.ico" />
+                <script
+                    async
+                    src={process.env.NEXT_PUBLIC_GOOGLE_ADS}
+                    crossorigin="anonymous"
+                ></script>
             </Head>
             <Header />
             <Welcome />
