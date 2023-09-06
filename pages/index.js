d2422ebb5200f0bd22077b692adaa646ec3bf798
wg326 <57165451+wg326@users.noreply.github.com>
2023-09-06 16:21:09 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Bottom from "../components/Bottom"
 import Header from "../components/Header"
 import Welcome from "../components/Welcome"
+import Script from "next/script"
 export default function Home() {
     return (
         <div>
@@ -9,26 +10,25 @@ export default function Home() {
                 <title>Testnet Minting Site</title>
                 <meta name="description" content="Ethereum testnet minting site" />
                 <link rel="icon" href="/Cjdowner-Cryptocurrency-Flat-Ethereum-ETH.ico" />
-                <script
-                    async
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-                ></script>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-                    `,
-                    }}
-                />
-                <script
-                    async
-                    src={process.env.NEXT_PUBLIC_GOOGLE_ADS}
-                    crossorigin="anonymous"
-                ></script>
             </Head>
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+                strategy="lazyOnload"
+            />
+            <Script strategy="lazyOnload">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
+            </Script>
+            <Script
+                src={process.env.NEXT_PUBLIC_GOOGLE_ADS}
+                crossOrigin="anonymous"
+                strategy="lazyOnload"
+            />
+
             <Header />
             <Welcome />
             <Bottom />
